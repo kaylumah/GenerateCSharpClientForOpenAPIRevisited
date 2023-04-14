c89b4cfb90f16b037e56e81f5ea36336348e0659
Max Hamulyák <max@kaylumah.nl>
2023-04-14 13:12:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Generate Csharp Client for Open API - Revisited
+
+<p align="center">
+  <img alt="Kaylumah Logo" width="460" height="300" src="logo.svg">
+</p>
+
+---
+
+## Description
+
+This repository contains the source code for my article "Generate C# client for OpenAPI - Revisited" which you can find [here](https://kaylumah.nl/2023/04/14/csharp-client-for-openapi-revistted.html).
+If you have any questions or comments about the repo or the blog, feel free to reach out over on twitter [@kaylumah](https://twitter.com/kaylumah).
+
+## License
+
+This repo is licensed under the [MIT License](LICENSE)
---
+++ src/Api/Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Kaylumah, 2023. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+internal sealed class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
